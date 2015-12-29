add8b4aa4e8e52cab2092c900b4282f297939b6d
playboy王某某 <wm5821090>
2015-12-30 01:55:57 +0800

    sms

--- cmd/cmdhandler.go
+++ cmd/cmdhandler.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"strings"
 	"ranbbService/models"
+	"io/ioutil"
+	"github.com/astaxie/beego/httplib"
 )
 
 var (
@@ -11,6 +13,7 @@ var (
 	_HELP = "help"
 	_SYNCDB = "SyncDB"
 	_PASSWD = "demon"
+	_SMS = "sms"
 )
 
 func Run() {
@@ -26,6 +29,8 @@ func handleCmd() {
 			printUseage()
 		case _SYNCDB :
 			syncDB()
+		case _SMS:
+			textSms()
 		}
 	}
 }
@@ -50,4 +55,20 @@ func syncDB() {
 	}else{
 		fmt.Print("密码错误")
 	}
+}
+
+func textSms() {
+	req := httplib.Get("http://apis.baidu.com/kingtto_media/106sms/106sms")
+	req.Header("apikey","e3b793a1d9278be02bf49a0f3f80b68a")
+	req.Param("mobile","18601761051")
+	req.Param("content","123456 【demon】")
+	rsp,err := req.SendOut()
+	if err != nil {
+		log.Info(err.Error())
+	}
+	data,err := ioutil.ReadAll(rsp.Body)
+	if err != nil {
+		log.Info(err.Error())
+	}
+	log.Info(string(data))
 }
